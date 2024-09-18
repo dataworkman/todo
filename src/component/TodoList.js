50bab5618874e5c793f356bebc56ae3bcc13a9bd
dataworkman <dataworkman@gmail.com>
2024-09-18 06:32:18 -0400

    todo

---
+++ src/component/TodoList.js
@@ -0,0 +1,19 @@
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+
+const TodoList = ({ todo }) => {
+  return (
+    <div className="TodoList">
+      <h4>Todo List 🌱</h4>
+      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <div className="list_wrapper">
+
+        {todo.map((it) => (
+          <TodoItem key={it.id} {...it} />
+        ))}
+
+      </div>
+    </div>
+  );
+};
+export default TodoList;
